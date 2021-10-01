26e95a738e6bda1b07dab485676150b1a473816e
fabio460 <82215067+fabio460@users.noreply.github.com>
2021-10-01 11:01:44 -0300

    Update crud_controller.php

--- app/Http/Controllers/crud_controller.php
+++ app/Http/Controllers/crud_controller.php
@@ -11,7 +11,7 @@ class crud_controller extends Controller
     {
         //return crud_model::all() fff;
         $res= DB::select('SELECT id,titulo,descricao FROM crud_models');
-        return $res;
+        return print_r($res);
     }
     public function store(Request $request)
     {
